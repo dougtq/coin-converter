073397770d77cef5b1c8185ba48b8ceed9960168
Douglas Alves <douglas.expalves@gmail.com>
2018-01-19 18:53:24 -0200

    add first exec test

---
+++ tests/main.spec.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai'
+const exec = require('child_process').exec
+
+const btcConverter = './src/main.js'
+
+describe('Main CLI', () => {
+  it('should return Hello World', (done) =>{
+    exec(btcConverter, (err, stdout, stderr) => {
+      if (err) throw err
+
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World')
+
+      done()
+    })
+  })
+})
