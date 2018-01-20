56880ec6c05ac48ea7b49200a757731650fa5900
Douglas Alves <douglas.expalves@gmail.com>
2018-01-20 16:37:26 -0200

    build output

---
+++ bin/main.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+'use strict';
+
+var program = require('commander');
+var pkg = require('../package.json');
+
+program.version(pkg.version).description(pkg.description).parse(process.argv);
