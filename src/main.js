004b7dc4bde187ad10980312bc28f112dbfd0a57
Douglas Alves <douglas.expalves@gmail.com>
2018-01-20 15:42:12 -0200

    add program versin on commander

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
-console.log('Hello World');
+const program = require('commander')
+const pkg = require('./../package.json')
+
+program
+  .version(pkg.version)
+  .parse(process.argv)
