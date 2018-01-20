722308fa70294d597d472403d91cff3b203f5979
Douglas Alves <douglas.expalves@gmail.com>
2018-01-20 15:41:51 -0200

    add cli version test

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,14 +1,17 @@
 import { expect } from 'chai'
 const exec = require('child_process').exec
 
+const pkg = require('./../package.json')
 const btcConverter = './src/main.js'
 
+
+
 describe('Main CLI', () => {
-  it('should return Hello World', (done) =>{
-    exec(btcConverter, (err, stdout, stderr) => {
+  it('should return version of btc-converter', (done) =>{
+    exec(`${btcConverter} -- version`, (err, stdout, stderr) => {
       if (err) throw err
 
-      expect(stdout.replace('\n', '')).to.be.equal('Hello World')
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version)
 
       done()
     })
