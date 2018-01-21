724a936e0ea3bc6f986dc2a94a2c26b3d8f8f8f7
Douglas Alves <douglas.expalves@gmail.com>
2018-01-20 22:40:49 -0200

    finish commander config

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 
 const program = require('commander');
 const pkg = require('../package.json');
-
+const convertBTC = require('./convertBTC');
 
 program
   .version(pkg.version)
   .description(pkg.description)
+  .option('-C, --currency <currency>', 'Currency to be converted { Default: USD }')
+  .option('-A, --amount <amount>', 'Amount to be converted { Default: 1 }')
   .parse(process.argv);
+
+convertBTC(program.currency, program.amount)
